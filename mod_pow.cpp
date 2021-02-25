96283ed56b113feadd145a00d110b0fce7b8d54c
greedtn <naoto.tsuda63@gmail.com>
2021-02-26 03:53:32 +0900

    mod_pow

---
+++ mod_pow.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (long long i = 0; i < (n); i++)
+using ll = long long;
+using P = pair<ll, ll>;
+
+// a^n % p を計算する関数
+ll mod_pow(ll a, ll n, ll mod) {
+    ll res = 1;
+    // n を 2進数に変換して考える
+    while (n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    cout << mod_pow(a, mod_pow(b, c, 4) + 4, 10) << endl;
+} 
