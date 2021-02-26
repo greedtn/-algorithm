93cd8b84359554dc6c53709e63012c3759635950
greedtn <naoto.tsuda63@gmail.com>
2021-02-26 09:45:41 +0900

    rec-dfs

---
+++ recursive_dfs.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (long long i = 0; i < (n); i++)
+using ll = long long;
+using P = pair<ll, ll>;
+
+using Graph = vector<vector<ll>>;
+
+// 深さ優先探索
+vector<bool> seen;
+void dfs(const Graph &G, int v) {
+    seen[v] = true; // v を訪問済にする
+
+    // v から行ける各頂点 next_v について
+    for (auto next_v: G[v]) {
+        if (seen[next_v]) continue; // 探索済なら飛ばす
+        dfs(G, next_v); // 再帰的に探索
+    }
+}
+
+int main() {
+
+    // 頂点と辺の数
+    ll n, m; cin >> n >> m;
+
+    // グラフの入力を受け取る (ここでは有向グラフを想定)
+    Graph G(n);
+    rep(i, m) {
+        ll a, b; cin >> a >> b;
+        a--;
+        b--;
+        G[a].push_back(b);
+    }
+
+    // 探索
+    seen.assign(n, false); // 初期状態では全ての頂点が未探索
+    for (ll v = 0; v < n; v++) {
+        if (seen[v]) continue;
+        dfs(G, v);
+    }
+}
